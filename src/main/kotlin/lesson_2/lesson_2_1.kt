dcf84712a5f393003caadce8ec9c23f108a84b8f
curio <attain06@gmail.com>
2024-07-12 18:45:54 +0300

    KS-2-1 solving for task 2-1

---
+++ src/main/kotlin/lesson_2/lesson_2_1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val grades = listOf(3, 4, 3, 5)
+
+    val average = grades.average()
+
+    println("Средний балл по анлийскому языку в классе: $average")
+}
