210110089e9d319c6d919ea42a99189f362f9d63
curio <attain06@gmail.com>
2024-07-12 14:11:15 +0300

    KS-1-5 delete comments, add constant for seconds and string/format

--- src/main/kotlin/lesson_1/lesson_1_file5.kt
+++ src/main/kotlin/lesson_1/lesson_1_file5.kt
@@ -1,23 +1,21 @@
 package org.example.lesson_1
 
-//Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
-//
-//- Переведи в коде количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные;
-//- Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
-//- Значения часов, минут и секунд отображать двумя цифрами.
 
 fun main() {
-    val seconds = 6480
 
-    val minutes = seconds / 60
+    val minutes = SECONDS / 60
 
-    val remainingSeconds = seconds % 60
+    val remainingSeconds = SECONDS % 60
 
     val hour = minutes / 60
 
     val remainingMinutes = minutes % 60
 
-    println("$hour:$remainingMinutes:$remainingSeconds"+"0")
+    val formattedTime = String.format("%02d:%02d:%02d", hour, remainingMinutes, remainingSeconds)
+    println(formattedTime)
+
 
 
 }
+
+const val SECONDS = 6480
