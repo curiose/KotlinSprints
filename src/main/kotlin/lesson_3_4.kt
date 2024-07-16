aba57c7a229fde229475f277bc45d2ca664aef7b
curio <attain06@gmail.com>
2024-07-16 14:29:35 +0300

    KS-3-4 - added solution to the task 3-4

---
+++ src/main/kotlin/lesson_3_4.kt
@@ -0,0 +1,15 @@
+package org.example
+
+fun main() {
+    var moveFrom = "E2"
+    var moveTo = "E4"
+    var moveNumber = 1
+
+    println("Сделан ход из $moveFrom в $moveTo. Номер хода: $moveNumber")
+
+    moveFrom = "D2"
+    moveTo = "D3"
+    moveNumber = 2
+
+    println("Сделан ход из $moveFrom в $moveTo. Номер хода: $moveNumber")
+}
