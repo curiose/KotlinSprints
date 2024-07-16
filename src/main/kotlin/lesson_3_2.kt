795dcc761b1f177f487dcd872c093ee8b051b90a
curio <attain06@gmail.com>
2024-07-16 12:18:01 +0300

    KS-3-2 - add solution to the task 3-2

---
+++ src/main/kotlin/lesson_3_2.kt
@@ -0,0 +1,17 @@
+package org.example
+
+fun main() {
+
+    val name = "Татьяна"
+    val secondName = "Сергеевна"
+    val surname = "Андреева"
+    var age = 20
+
+    println("Данные пользователя: $surname $name $secondName, возвраст: $age лет")
+
+    val newSurname = "Сидорова"
+    age = 22
+
+    println("Данные пользователя: $newSurname $name $secondName, возвраст: $age года")
+
+}
